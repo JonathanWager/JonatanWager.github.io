937a638397755bb0743b0ad14a6085797d145680
Jonathan <storfot32@gmail.com>
2022-09-23 12:20:50 +0200

    commit3

--- test.js
+++ test.js
@@ -1,28 +1,3 @@
-var character = document.getElementById("you");
-var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){
-        return
-    }
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },500);
-}
-var gameOver = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=220){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("score").innerHTML = Math.floor(counter/100);
-    }
-}, 10);
 
 function myFunction(){
     let name;
